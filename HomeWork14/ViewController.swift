bccbe3400a389c3f03e69d576121c71092a0776a
Igor Nikolaev <igor01131987@gmail.com>
2023-01-18 14:13:57 +0300

    Initial Commit

---
+++ HomeWork14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork14
+//
+//  Created by Игорь Николаев on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
